c594ef36099995af508645a6b0c6e6bbe5a82c6e
XR2Niatelkom <niaagrelya78@gmail.com>
2023-02-07 14:28:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,170 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const cors = require("cors")
-const mysql = require("mysql")
-const moment = require("moment")
-
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "penyewaan mobil"
-})
-
-db.connect(error => {
-    if(error){
-        console.log(error.message)
+const express = require("express") 
+const multer = require("multer") //untuk upload file 
+const path = require("path") // untuk memanggil path direktori 
+const fs = require("fs") //untuk manajemen file 
+const bodyParser = require("body-parser") 
+const cors = require("cors") 
+const mysql = require("mysql") 
+const moment = require("moment") 
+ 
+const app = express() 
+app.use(express.json()) 
+app.use(express.static(__dirname)); 
+app.use(cors()) 
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({ extended: true })) 
+ 
+const storage = multer.diskStorage({ 
+    destination: (req, file, cb) => { 
+        //set file storage 
+        cb(null, './image'); 
+    }, 
+    filename: (req, file, cb) => { 
+        //generate file name 
+        cb(null, "image-"+ Date.now() + path.extname(file.originalname)) 
     } 
-    else {
-        console.log("MySQL Connected")
-    }
+}) 
+let upload = multer({storage: storage}) 
+ 
+const db = mysql.createConnection({ 
+    host: "localhost", 
+    user: "root", 
+    password: "", 
+    database: "penyewaan_mobil" 
+}) 
+ 
+ 
+db.connect(error => { 
+    if (error) { 
+        console.log(error.message) 
+    } else { 
+        console.log("MySQL Connected") 
+    } 
+}) 
+ 
+//--------------------------------------- Menambah Data Mobil -------------------------------------------- 
+ 
+app.post("/mobil", upload.single("image"), (req, res) => { 
+    let data = { 
+        nomor_mobil: req.body.nomor_mobil, 
+        merk: req.body.merk, 
+        jenis: req.body.jenis, 
+        warna: req.body.warna, 
+        tahun_pembuatan: req.body.tahun_pembuatan, 
+        biaya_sewa_per_hari: req.body.biaya_sewa_per_hari, 
+        image: req.file.filename 
+    } 
+ 
+    if (!req.file) { 
+        //jika tidak ada file yang diupload 
+        res.json({ 
+            message: "Tidak ada file yang dikirim" 
+        }) 
+    } else { 
+        let sql = "insert into mobil set ?" 
+ 
+        db.query(sql, data, (error, result) => { 
+            if(error) throw error 
+            res.json({ 
+                message: result.affectedRows + " data berhasil disimpan" 
+            }) 
+        }) 
+    } 
+}) 
+ 
+ 
+app.put("/mobil", upload.single("image"), (req, res) => { 
+    let data = null, sql = null 
+    let param = { id_mobil: req.body.id_mobil } 
+     
+    if(!req.file) { 
+        data = { 
+            nomor_mobil: req.body.nomor_mobil, 
+            merk: req.body.merk, 
+            jenis: req.body.jenis, 
+            warna: req.body.warna, 
+            tahun_pembuatan: req.body.tahun_pembuatan, 
+            biaya_sewa_per_hari: req.body.biaya_sewa_per_hari 
+        } 
+    } else { 
+        data = { 
+            nomor_mobil: req.body.nomor_mobil, 
+            merk: req.body.merk, 
+            jenis: req.body.jenis, 
+            warna: req.body.warna, 
+            tahun_pembuatan: req.body.tahun_pembuatan, 
+            biaya_sewa_per_hari: req.body.biaya_sewa_per_hari, 
+            image: req.file.filename 
+        } 
+        sql = "select * from mobil where ?" 
+ 
+        db.query(sql, param, (err, result) => { 
+            if (err) throw err 
+ 
+            let fileName = result[0].image 
+ 
+            let dir = path.join(__dirname, "image", fileName) 
+            fs.unlink(dir, (error) => { 
+            }) 
+        }) 
+    } 
+    sql = "update mobil set ? where ?" 
+ 
+    db.query(sql, [data,param], (error, result) => { 
+        if (error) { 
+            res.json({ 
+                message: error.message 
+            }) 
+        } else { 
+            res.json({ 
+                message: result.affectedRows + " data berhasil diubah" 
+            }) 
+        } 
+    }) 
+ }) 
+ 
+app.delete("/mobil/:id_mobil", (req, res) => { 
+    let param = {id_mobil: req.params.id_mobil} 
+ 
+    let sql = "select * from mobil where ?" 
+ 
+    db.query(sql, param, (error, result) => { 
+        if (error) throw error 
+ 
+        let fileName = result[0].image 
+ 
+        let dir = path.join(__dirname, "image", fileName) 
+        fs.unlink(dir, (error) => {}) 
+}) 
+    sql = "delete from mobil where ?" 
+    db.query(sql, param, (error, result) => { 
+        if (error) { 
+            res.json({ 
+                message: error.message 
+            }) 
+        } else { 
+            res.json({ 
+                message: result.affectedRows + " data berhasil dihapus" 
+            }) 
+        } 
+})     
+    }) 
+ 
+ 
+app.get("/mobil", (req, res) => { 
+ 
+    let sql = "select * from mobil" 
+ 
+    // run query 
+    db.query(sql, (error, result) => { 
+        if (error) throw error 
+        res.json({ 
+            data: result, 
+            count: result.length 
+        }) 
+    }) 
 })
+
 
 app.get("/pelanggaan", (req,res) => {
     let sql = "select * from pelanggaan"
@@ -260,6 +401,8 @@ app.delete("/karyawan/:id_karyawan", (req,res) => {
 
 
 
-app.listen(7000,() => {
+
+
+app.listen(8000,() => {
     console.log("bismillah")
 })
